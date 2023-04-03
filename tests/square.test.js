d9c487a14fa9be83244a3d18019a0ade0097a435
Cody <bcuy46@yahoo.com>
2023-04-03 17:06:27 -0400

    updated some code after troubleshooting, still getting error message to continually add closing svg tags. Will continue to debug

--- tests/square.test.js
+++ tests/square.test.js
@@ -1,21 +1,21 @@
-const Square = require('../lib/square');
+const Square = require("../lib/square");
 
-describe('Square class', () => {
-  test('renders correctly with default values', () => {
+describe("Square class", () => {
+  test("renders correctly with default values", () => {
     const square = new Square();
     const expectedOutput = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-        <rect x="50" y="50" width="100" height="100" fill="black"/>
+        <rect x="50" y="50" width="100" height="100" fill=black/>
         <text x="150" y="125" font-size="60" text-anchor="middle" fill="black"></text>
       </svg>`;
     expect(square.render()).toBe(expectedOutput);
   });
 
-  test('renders correctly with custom values', () => {
-    const square = new Square('red', 'white', 'Hello');
+  test("renders correctly with custom values", () => {
+    const square = new Square("red", "white", "Hello");
     const expectedOutput = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-    <rect x="50" y="50" width="100" height="100" fill="red"/>
-    <text x="150" y="125" font-size="63.245553203367585" text-anchor="middle" fill="white">Hello</text>
-    </svg>`;
+        <rect x="50" y="50" width="100" height="100" fill="red"/>
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">Hello</text>
+      </svg>`; // Add closing </svg> tag
     expect(square.render()).toBe(expectedOutput);
   });
 });
